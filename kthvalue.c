d3fa037af676bfac0a89c047dfaf0ffae41f66a0
santhoshkv07 <37010258+santhoshkv07@users.noreply.github.com>
2019-02-26 19:49:26 +0530

    Create kthvalue.c

---
+++ kthvalue.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+  int n,k,x[100];
+  scanf("%d %d",&n,&k);
+  for(int i=0;i<n;i++)
+  {
+    scanf("%d",&x[i]);
+  }
+  for(int i=0;i<n;i++)
+  {
+    if(x[i]==k)
+    {
+      printf("%d",x[i]);
+    }
+  }
+  return 0;
+}
