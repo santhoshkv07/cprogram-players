eb711866e84b45798b25894008139838ba5aaa26
santhoshkv07 <37010258+santhoshkv07@users.noreply.github.com>
2019-02-26 16:11:52 +0530

    Create romanletter.c

---
+++ romanletter.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+int main()
+{
+char s[10];
+"enter the roman letter";
+scanf("%s",&s[10]);
+switch(s)
+case 'I':
+   printf("1");
+   break;
+case 'II':
+   printf("1");
+   break;
+case 'III':
+   printf("1");
+   break;   
+case 'IV':
+   printf("1");
+   break;
+case 'V':
+   printf("1");
+   break;
+case 'VI':
+   printf("1");
+   break;
+case 'VII':
+   printf("1");
+   break;
+case 'VIII':
+   printf("1");
+   break;
+case 'IX':
+   printf("1");
+   break;
+case 'X':
+   printf("1");
+   break;
+case 'XI':
+   printf("1");
+   break;
+case 'XII':
+   printf("1");
+   break;
+case 'XIII':
+   printf("1");
+   break;
+case 'XIV':
+   printf("1");
+   break;
+case 'XV':
+   printf("1");
+   break;
+case 'XVI':
+   printf("1");
+   break;
+case 'XVII':
+   printf("1");
+   break;
+case 'XVIII':
+   printf("1");
+   break;
+case 'XIX':
+   printf("1");
+   break;
+case 'XX':
+   printf("1");
+   break;  
+return 0;
+}
