6152bd914263b9cd1d21c4177a39db94ef64b7f6
santhoshkv07 <37010258+santhoshkv07@users.noreply.github.com>
2019-02-26 19:36:05 +0530

    Create evenfactorsofnum.c

---
+++ evenfactorsofnum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int n;
+  scanf("%d",&n);
+  for(int i=1;i<=n;i++)
+  {
+    if(i%2==0)
+    {
+     if(n%i==0)
+     {
+       printf("%d\t",i);
+     }
+    }
+  }
+  return 0;
+}
