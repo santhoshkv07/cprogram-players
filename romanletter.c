d54e56174513c43ff538b95772e6cd3decad1359
santhoshkv07 <37010258+santhoshkv07@users.noreply.github.com>
2019-02-28 06:47:44 +0530

    Update romanletter.c

--- romanletter.c
+++ romanletter.c
@@ -1,79 +1,40 @@
 #include <stdio.h>
- 
-void predigit(char num1, char num2);
-void postdigit(char c, int n);
- 
-char romanval[1000];
-int i = 0;
-int main()
+#include<string.h>
+int main() 
 {
-    int j;
-    long number;
- 
-    printf("Enter the number: ");
-    scanf("%d", &number);
-    if (number <= 0)
+    int x,i,res,b[20],integer=0;
+    char a[10];
+    scanf("%s",a);
+    x=strlen(a);
+    for(i=0;i<x;i++)
     {
-        printf("Invalid number");
-        return 0;
+    	if(a[i]=='I')
+    	b[i]=1;
+    	else if(a[i]=='V')
+    	b[i]=5;
+    	else if(a[i]=='X')
+    	b[i]=10;
+    	else if(a[i]=='L')
+    	b[i]=50;
+    	else if(a[i]=='C')
+    	b[i]=100;
+    	else if(a[i]=='D')
+    	b[i]=500;
+    	else
+    	printf("INVALID");
     }
-    while (number != 0 && number>20)
+    for(i=0;i<x;i=i+2)
     {
-        if (number >= 10)
-        {
-            if (number < (10 + 3 * 10))
-            {
-                postdigit('X', number / 10);
-                number = number - (number / 10) * 10;
-            }
-            else
-            {
-                predigit('X','L');
-                number = number - (50 - 10);
-            }
+    	if(b[i]<b[i+1])
+    	{
+    	    res=b[i+1]-b[i];
         }
-        else if (number >= 5)
+    	else 
         {
-            if (number < (5 + 4 * 1))
-            {
-                postdigit('V', number / 5);
-                number = number - (number / 5) * 5;
-            }
-            else
-            {
-                predigit('I', 'X');
-                number = number - (10 - 1);
-            }
+        	res=b[i]+b[i+1];
         }
-        else if (number >= 1)
-        {
-            if (number < 4)
-            {
-                postdigit('I', number / 1);
-                number = number - (number / 1) * 1;
-            }
-            else
-            {
-                predigit('I', 'V');
-                number = number - (5 - 1);
-            }
-        }
+    integer=res+integer;
     }
-    printf("Roman number is: ");
-    for(j = 0; j < i; j++)
-        printf("%c", romanval[j]);
+    printf("%d",integer);
     return 0;
-}
- 
-void predigit(char num1, char num2)
-{
-    romanval[i++] = num1;
-    romanval[i++] = num2;
-}
- 
-void postdigit(char c, int n)
-{
-    int j;
-    for (j = 0; j < n; j++)
-        romanval[i++] = c;
 }
