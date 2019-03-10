1c1e0fc015459245388d5518a7d62f3dfc0a5f95
santhoshkv07 <37010258+santhoshkv07@users.noreply.github.com>
2019-03-10 18:53:38 +0530

    Add files via upload

---
+++ length of the correaponding binarydigit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int num,rem,a[30],i,count=0;
+	scanf("%d",&num);
+	while(num!=0)
+	{
+		rem=num%2;
+		a[i]=rem;
+		i++;
+		count++;
+		num=num/2;
+	}
+	printf("%d",count);
+	return 0;
+}
