373d074cb985d70b773450170d8885f133ed6cd4
santhoshkv07 <37010258+santhoshkv07@users.noreply.github.com>
2019-03-10 13:36:55 +0530

    Update string count.c

--- string count.c
+++ string count.c
@@ -1,41 +1,13 @@
 #include <stdio.h>
-#include <string.h>
-#define MAX_SIZE 100000
-int count(char * str, char * str1);
-int main()
+int main() 
 {
-    char str[MAX_SIZE];
-    char str1[MAX_SIZE];
-    int c;
-    gets(str);
-    gets(str1);
-    c = count(str, str1);
-    printf("%d",c);
-    return 0;
-}
-int count(char * str, char * str1)
-{
-    int i, j, f, c;
-    int l, l1;
-   l = strlen(str);     
-   l1 = strlen(str1);
-    c = 0;
-    for(i=0; i <= l-l1; i++)
-   {
-        f= 1;
-        for(j=0; j<l1; j++)
-        {
-            if(str[i + j] != str1[j])
-            {
-                f = 0;
-                break;
-            }
-        }
-        if(f == 1)
-        {
-            c++;
-        }
-    }
-    printf("%d",c);
-    return 0;
+	char str[30];
+	int i,count=0;
+	scanf("%s",str);
+	for(i=0;str[i]!='\0';i++)
+	{
+		count=count+1;
+	}
+	printf("%d",count);
+	return 0;
 }
