bd5403bb4bf4de5ae013fee8a2608390eff6a28d
santhoshkv07 <37010258+santhoshkv07@users.noreply.github.com>
2019-03-10 15:00:54 +0530

    Update print num less than n.c

--- print num less than n.c
+++ print num less than n.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 int main() 
 {
-	int n;
-	scanf("%d",&n);
-	int array[n],i;
+	int n,k;
+	scanf("%d %d",&n,&k);
+	int array[n],i,j,temp;
 	for(i=0;i<n;i++)
 	{
-		scanf("%d ",&array[i]);
+		scanf("%d",&array[i]);
+	}	
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+		if(array[i]>array[j])
+		{
+			temp=array[i];
+			array[i]=array[j];
+			array[j]=temp;
+		}
+		}
 	}
 	for(i=0;i<n;i++)
 	{
-		if(array[i]<n)
+		if(array[i]<k)
 		{
 			printf("%d ",array[i]);
 		}
