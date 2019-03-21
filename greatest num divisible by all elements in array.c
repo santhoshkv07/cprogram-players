ece941d3b24bd04599948fbabf47795df8575968
santhoshkv07 <37010258+santhoshkv07@users.noreply.github.com>
2019-03-21 20:04:20 +0530

    Add files via upload

---
+++ greatest num divisible by all elements in array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+ int N,a[100],i,j,c=0;
+ scanf("%d",&N);
+ for(i=0;i<N;i++)
+ {
+    scanf("%d",&a[i]);
+ }
+ for(i=2;;i++)
+ {
+    c=0;
+    for(j=0;j<N;j++)
+    {
+        if(i%a[j]==0)
+        {
+           c++;
+        } 
+    }   
+    if(c==N)
+    {
+    printf("%d",i);
+    break;
+    }
+ }
+    return 0;
+}
